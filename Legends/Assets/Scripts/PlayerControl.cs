3c745072047c2fbb8db279d492cee4392ea20640
ArthGIT0 <arturumbrashko@gmail.com>
2025-04-10 14:26:41 +0300

    second commit updates

---
+++ Legends/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float moveSpeed = 7;
+
+    private CharacterController characterController;
+    private Vector3 targetPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distToTarget = Vector3.Distance(targetPosition, transform.position);
+        if (distToTarget > 1)
+        {
+            Vector3 direction = Vector3.Normalize(targetPosition - transform.position);
+            characterController.Move(direction * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 500, layerMask))
+            {
+                targetPosition = hit.point;
+                transform.LookAt(targetPosition);
+            }
+        }
+    }
+}
